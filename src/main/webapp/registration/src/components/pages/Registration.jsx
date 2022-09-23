daa8f7e36a096df7ee3198ee9fc0ef3580de7510
Rafaela Inácio <rafaelafsinacio@gmail.com>
2022-09-23 12:26:30 -0300

    adding message

--- src/main/webapp/registration/src/components/pages/Registration.jsx
+++ src/main/webapp/registration/src/components/pages/Registration.jsx
@@ -1,26 +1,47 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 
 import Form from '../form/Form'
+import Message from '../form/Message'
 
 
 const Registration = () => {
 
+    const [message, setMessage] = useState('')
+    const [type, setType] = useState('success')
+
     const createPerson = (data) => {
         
+    
         fetch('http://localhost:8080/people', {
             method: "POST",
             headers: {
+                "Accept": "application/json, text/plain, */*",
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(data)
-        }).then((resp) => resp.json())
-        .then(() => console.log("this person was created succesfully"))
-        .catch((err) => console.error(err))
+        })
+            .then(response => {
+                if(response.ok) {
+                    response.json()
+                } else {
+                    throw new Error("Something went wrong.")
+                }
+            })
+            .then(() => {
+                setMessage("New account added successfully!")
+                setType('success')
+            })
+            .catch((err) => {
+                alert(`Something went wrong: ${err}`)
+
+            })
     }
 
     return (
         <div>
             <Title>create account</Title>
+            {message && <Message type={type} msg={message}/> }
             <Form handleSubmit={createPerson}/>
         </div>
 
